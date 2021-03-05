fe6afbac8abb5edb4067e639555220d1861e639e
Michael <michaelran16@gmail.com>
2021-03-05 10:31:45 +0800

    format

--- transaction-service/src/main/java/services/transaction/HelloEthMain.java
+++ transaction-service/src/main/java/services/transaction/HelloEthMain.java
@@ -1,36 +1,24 @@
 package services.transaction;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import org.web3j.crypto.*;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
 import org.web3j.protocol.Web3j;
-import org.web3j.protocol.admin.Admin;
-import org.web3j.protocol.admin.methods.response.PersonalUnlockAccount;
 import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.protocol.core.methods.response.EthAccounts;
-import org.web3j.protocol.core.methods.response.EthCoinbase;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
-import org.web3j.protocol.core.methods.response.EthGetTransactionReceipt;
 import org.web3j.protocol.core.methods.response.EthSendTransaction;
-import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
-
-import org.web3j.utils.Convert;
-import org.web3j.utils.Numeric;
 
 public class HelloEthMain {
 
@@ -40,12 +28,14 @@ public class HelloEthMain {
     private static String toAddress = "0x75D6c523107AC20351A9Bb55dCbe7e277f661318";
     private static Credentials fromCredentials = Credentials.create("45503b4549ce1bcc5fe8622370994132da1b9458bc4d795410fd0ee0d6aaceb2");
 
-    public static void main(String[] args) throws CipherException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         System.out.println("start hello eth");
 
         Web3j web3j = Web3j.build(new HttpService(WEB3_ADDRESS));
 
         // 查询 ETH 余额
+        // https://www.jianshu.com/p/d6dc974d9748
+
         EthGetBalance ethGetBalance = null;
         try {
             ethGetBalance = web3j.ethGetBalance(
@@ -59,9 +49,16 @@ public class HelloEthMain {
         System.out.println("eth get balance is " + balance);
 
         // 以太坊 转账
-
-        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(
-                fromAddress, DefaultBlockParameterName.LATEST).sendAsync().get();
+        // https://www.jianshu.com/p/8ae984e6bafc
+        EthGetTransactionCount ethGetTransactionCount = null;
+        try {
+            ethGetTransactionCount = web3j.ethGetTransactionCount(
+                    fromAddress, DefaultBlockParameterName.LATEST).sendAsync().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
 
         BigInteger nonce = ethGetTransactionCount.getTransactionCount();
 
@@ -70,18 +67,26 @@ public class HelloEthMain {
                 Convert.toWei("18", Convert.Unit.GWEI).toBigInteger(),
                 Convert.toWei("45000", Convert.Unit.WEI).toBigInteger(),
                 toAddress,
-                BigInteger.valueOf(20_000_000_000L)
+                Convert.toWei("0.20210305", Convert.Unit.ETHER).toBigInteger()
         );
         byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, fromCredentials);
         String hexValue = Numeric.toHexString(signedMessage);
 
-        EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
-        if (ethSendTransaction.hasError()) {
-            System.out.println("transfer error:" + ethSendTransaction.getError().getMessage());
-        } else {
-            String transactionHash = ethSendTransaction.getTransactionHash();
-            System.out.println("Transfer transactionHash:" + transactionHash);
+        EthSendTransaction ethSendTransaction = null;
+        try {
+            ethSendTransaction = web3j.ethSendRawTransaction(hexValue).sendAsync().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
-    }
 
+        if (ethSendTransaction.hasError()) {
+                System.out.println("transfer error:" + ethSendTransaction.getError().getMessage());
+            } else {
+                String transactionHash = ethSendTransaction.getTransactionHash();
+                System.out.println("Transfer transactionHash:" + transactionHash);
+            }
+
+    }
 }
